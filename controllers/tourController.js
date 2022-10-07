761371e9e058a40d0e8a7832e9ba9168ee33072b
mma-90 <moh.mag.ali@gmail.com>
2022-10-07 04:17:19 +0200

    tour crud controller

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -3,7 +3,7 @@ import Tour from "./../models/tourModel.js";
 export const getAllTours = async (req, res) => {
   try {
     const tours = await Tour.find({});
-    res.status(200).json({ tours });
+    res.status(200).json({ status: "success", results: tours.length, tours });
   } catch (error) {
     res.status(500).send(error);
   }
@@ -12,7 +12,49 @@ export const getAllTours = async (req, res) => {
 export const createTour = async (req, res) => {
   try {
     const tour = await Tour.create(req.body);
-    res.status(201).json({ tour });
+    res.status(201).json({ status: "success", tour });
+  } catch (error) {
+    res.status(400).json({
+      status: "fail",
+      message: error,
+    });
+  }
+};
+
+export const getTour = async (req, res) => {
+  try {
+    const tour = await Tour.findById(req.params.id);
+    res.status(200).json({ status: "success", tour });
+  } catch (error) {
+    res.status(400).json({
+      status: "fail",
+      message: error,
+    });
+  }
+};
+
+export const deleteTour = async (req, res) => {
+  try {
+    await Tour.findByIdAndDelete(req.params.id);
+
+    res.status(204).send("deleted");
+  } catch (error) {
+    res.status(400).json({
+      status: "fail",
+      message: error,
+    });
+  }
+};
+
+export const updateTour = async (req, res) => {
+  try {
+    console.log(req.body);
+    const tour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+
+    res.status(202).json({ status: "success", tour });
   } catch (error) {
     res.status(400).json({
       status: "fail",
--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -1,10 +1,18 @@
 import express from "express";
 import * as tourController from "./../controllers/tourController.js";
+import checkBody from "../middlewares/checkBody.js";
+
 const router = express.Router();
 
 router
   .route("/")
   .get(tourController.getAllTours)
-  .post(tourController.createTour);
+  .post(checkBody, tourController.createTour);
+
+router
+  .route("/:id")
+  .get(tourController.getTour)
+  .delete(tourController.deleteTour)
+  .patch(checkBody, tourController.updateTour);
 
 export default router;
