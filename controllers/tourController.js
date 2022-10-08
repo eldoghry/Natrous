cb87491b31393875c206157c202d54897176fcba
mma-90 <moh.mag.ali@gmail.com>
2022-10-08 16:03:46 +0200

    api Features class

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,47 +1,19 @@
+import APIFeatures from "../utils/apiFeatures.js";
 import Tour from "./../models/tourModel.js";
+
+
 
 export const getAllTours = async (req, res) => {
   try {
     // 1) filtering
-    let filters = { ...req.query };
-    const excludes = ["sort", "limit", "page", "fields"];
-    excludes.forEach((el) => delete filters[el]);
 
-    let query;
+    const apiFeatures = new APIFeatures(Tour.find(), req.query)
+      .filter()
+      .limitFields()
+      .sort()
+      .paginate();
 
-    if (filters) {
-      //advanced filtering { difficulty: 'easy', price: { $gte: '500' } }
-      //http://localhost:3000/api/v1/tours?difficulty=easy&price[gte]=500 => { difficulty: 'easy', price: { gte: '500' } }
-
-      let filterStr = JSON.stringify(filters);
-      filterStr = filterStr.replace(
-        /\b(gte|gt|lte|lt)\b/g,
-        (match) => `$${match}`
-      );
-      filters = JSON.parse(filterStr);
-
-      query = Tour.find(filters);
-    } else {
-      query = Tour.find({});
-    }
-
-    //2) sorting
-    if (req.query.sort) {
-      const sortBy = req.query.sort.split(",").join(" ");
-      query = query.sort(sortBy);
-    } else {
-      query = query.sort("-createdAt"); //newest first
-    }
-
-    //3)limiting selected fields;
-    if (req.query.fields) {
-      const fields = req.query.fields.split(",");
-      query = query.select(fields);
-    } else {
-      query = query.select(["-__v"]); //remove unnessasry __v
-    }
-
-    const tours = await query;
+    const tours = await apiFeatures.query;
 
     res.status(200).json({
       status: "success",
@@ -50,6 +22,7 @@ export const getAllTours = async (req, res) => {
       tours,
     });
   } catch (error) {
+    console.log(error);
     res.status(500).send(error);
   }
 };
---
+++ utils/apiFeatures.js
@@ -0,0 +1,62 @@
+export default class APIFeatures {
+  //query: MongoQuery , queryStr: express req query
+  constructor(query, queryStr) {
+    this.query = query;
+    this.queryStr = queryStr;
+  }
+
+  filter() {
+    let filters = { ...this.queryStr };
+    const excludes = ["sort", "limit", "page", "fields"];
+    excludes.forEach((el) => delete filters[el]);
+
+    if (filters) {
+      //advanced filtering { difficulty: 'easy', price: { $gte: '500' } }
+      //http://localhost:3000/api/v1/tours?difficulty=easy&price[gte]=500 => { difficulty: 'easy', price: { gte: '500' } }
+
+      let filterStr = JSON.stringify(filters);
+      filterStr = filterStr.replace(
+        /\b(gte|gt|lte|lt)\b/g,
+        (match) => `$${match}`
+      );
+      filters = JSON.parse(filterStr);
+      this.query = this.query.find(filters);
+    }
+
+    return this;
+  }
+
+  sort() {
+    if (this.queryStr.sort) {
+      const sortBy = this.queryStr.sort.split(",").join(" ");
+      this.query = this.query.sort(sortBy);
+    } else {
+      this.query = this.query.sort("-createdAt"); //newest first
+    }
+    return this;
+  }
+
+  limitFields() {
+    if (this.queryStr.fields) {
+      const fields = this.queryStr.fields.split(",");
+      this.query = this.query.select(fields);
+    } else {
+      this.query = this.query.select(["-__v"]); //remove unnessasry __v
+    }
+
+    return this;
+  }
+
+  paginate() {
+    //limit = 10 page 1 : [1 - 10], page 2 : [11 - 20], page 3 : [21 - 30]
+
+    if (this.queryStr.page) {
+      const limit = +this.queryStr.limit || 10;
+      const page = +this.queryStr.page || 1;
+
+      this.query = this.query.skip((page - 1) * limit).limit(limit);
+    }
+
+    return this;
+  }
+}
