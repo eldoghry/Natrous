b58fe5a552151e3bb9671fb60b27b6067cac9c37
mma-90 <moh.mag.ali@gmail.com>
2022-10-22 04:17:20 +0200

    implement index /

--- controllers/viewController.js
+++ controllers/viewController.js
@@ -1,10 +1,14 @@
-// import catchAsync from "../utils/catchAsync.js";
+import catchAsync from "../utils/catchAsync.js";
 // import AppError from "../utils/AppError.js";
 // import * as factory from "./handlerFactory.js";
 import path from "path";
+import Tour from "./../models/tourModel.js";
 
-export const getOverview = (req, res) => {
-  res.status(200).render(path.join(process.cwd(), "views", "base.pug"), {
-    title: "title string",
+export const getOverview = catchAsync(async (req, res) => {
+  const tours = await Tour.find();
+
+  res.status(200).render(path.join(process.cwd(), "views", "index.pug"), {
+    title: "All tours",
+    tours,
   });
-};
+});
